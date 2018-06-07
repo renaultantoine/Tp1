d5ff2cb8219a75486034c4c9299ff58ca58e6386
antoinerenault53 <antoine.renault53@laposte.net>
2018-06-07 12:29:17 +0200

    Finally

--- src/main/java/agile/Application.java
+++ src/main/java/agile/Application.java
@@ -1,27 +1,38 @@
 
-
 package agile;
+
 import org.joda.time.DateTime;
 
-/**Entry point of application.**/
-public class Application{
+/** Entry point of application. **/
+public class Application {
 
 	/**
-	 * Methode main 
-	 * @param args tab of args
+	 * Private Application main
 	 */
-	public static void main(final String[] args)
-	{
+	private Application() {
+		super();
+	}
+
+	/**
+	 * Methode main
+	 * 
+	 * @param args
+	 *            tab of args
+	 */
+	public static void main(final String[] args) {
 		System.out.println("Hello world !!!");
 		final DateTime date = DateTime.now();
 		System.out.println(date);
-		
+
 	}
-	
-	/** 
+
+	/**
 	 * Add methode with 2 parameter.
-	 * @param i int
-	 * @param j int
+	 * 
+	 * @param i
+	 *            int
+	 * @param j
+	 *            int
 	 * @return int
 	 */
 	public static int addFunctionCalculator(final int entierUn, final int entierDeux) {
