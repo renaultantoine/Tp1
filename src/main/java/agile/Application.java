8b218fb7accb8cf974bf99bbeeed01fa861b915f
antoinerenault53 <antoine.renault53@laposte.net>
2018-06-07 11:34:56 +0200

    Correct

--- src/main/java/agile/Application.java
+++ src/main/java/agile/Application.java
@@ -1,7 +1,11 @@
+
+
 package agile;
 import org.joda.time.DateTime;
 
+/**Entry point of application.**/
 public class Application{
+
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world !!!");
@@ -10,6 +14,12 @@ public class Application{
 		
 	}
 	
+	/** 
+	 * Add methode with 2 parameter.
+	 * @param i int
+	 * @param j int
+	 * @return int
+	 */
 	public static int add(int i, int j) {
 		return i + j;
 	}
--- src/test/java/agile/ApplicationTest.java
+++ src/test/java/agile/ApplicationTest.java
@@ -1,4 +1,5 @@
 package agile;
+
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -6,17 +7,15 @@ import org.junit.Test;
 import agile.Application;
 import junit.framework.Assert;
 
-
 public class ApplicationTest {
-
 
 	@Test
 	public void testAdd() {
 		int i = 2;
-	    int j = 3;
-	    
-	    int k = Application.add(i , j);
-	    
-	    Assert.assertEquals("Add not good", k, 5);
+		int j = 3;
+		int k = Application.add(i, j);
+
+		Assert.assertEquals("Add not good", k, 5);
 	}
 }
+ 
