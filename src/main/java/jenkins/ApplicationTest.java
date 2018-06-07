a53676baff586a0eb6611a62c1b0962c6f4d2d8c
antoinerenault53 <antoine.renault53@laposte.net>
2018-06-07 09:29:12 +0200

    Correction

--- src/main/java/jenkins/ApplicationTest.java
+++ src/main/java/jenkins/ApplicationTest.java
@@ -3,10 +3,12 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import agile.Application;
 import junit.framework.Assert;
 
 
 public class ApplicationTest {
+
 
 	@Test
 	public void testAdd() {
@@ -15,6 +17,6 @@ public class ApplicationTest {
 	    
 	    int k = Application.add(i , j);
 	    
-	    Assert.assertEquals("Add not good", k, 3);
+	    Assert.assertEquals("Add not good", k, 5);
 	}
 }
